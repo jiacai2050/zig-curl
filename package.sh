cf7a049bd18316a2f38ba54f66106ab7a442fdb8
jiacai2050 <dev@liujiacai.net>
2024-01-21 19:21:43 +0800

    Update install

--- package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-# rm -rf /tmp/zig-curl.zip
-# zip -r  /tmp/zig-curl.zip /*.zig src build.zig build.zig.zon libs \
-#     -x /*DS_Store
-
-rm -rf /tmp/zig-curl.tar.gz
-tar --exclude='.DS_Store ' -czf /tmp/zig-curl.tar.gz *.zig src build.zig build.zig.zon libs
