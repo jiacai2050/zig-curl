3004557bcbcc80d1e232295263e53eb692310764
jiacai2050 <dev@liujiacai.net>
2024-07-05 08:43:28 +0800

    bump zig to 0.13.0 in CI

--- build.zig
+++ build.zig
@@ -47,6 +47,20 @@ pub fn build(b: *Build) void {
     const run_main_tests = b.addRunArtifact(main_tests);
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
+
+    const check_step = b.step("check", "Used for checking the library");
+    inline for (.{ "basic", "advanced", "multi" }) |name| {
+        const check_exe = b.addExecutable(.{
+            .name = "check-" ++ name,
+            .root_source_file = b.path("examples/" ++ name ++ ".zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        });
+
+        check_exe.root_module.addImport(MODULE_NAME, module);
+        check_step.dependOn(&check_exe.step);
+    }
 }
 
 fn buildLibcurl(
